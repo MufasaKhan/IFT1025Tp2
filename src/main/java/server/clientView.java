2c34d115c6d655d18d11038deaac77670b1ad9ab
Emmanuel <emmanuel77fg@yahoo.fr>
2023-04-14 23:32:58 -0400

    gui complet. Ajouter fonction inscrire

--- src/main/java/server/clientView.java
+++ src/main/java/server/clientView.java
@@ -3,15 +3,14 @@ package server;
 import javafx.collections.FXCollections;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import server.models.Course;
+import server.models.RegistrationForm;
 
 import java.util.ArrayList;
 
@@ -22,9 +21,19 @@ public class clientView {
     private final Scene scene;
     private Course course;
     private Controleur controleur =  new Controleur();
+    TextField prenom = new TextField();
+    TextField nom = new TextField();
+    TextField email = new TextField();
+    TextField matricule = new TextField();
+    VBox informations = new VBox(10);
+    Label pr = new Label("Prenom");
+    Label na = new Label("Nom");
+    Label em = new Label("Email");
+    Label ma = new Label("Matricule");
 
     private final StackPane root = new StackPane();
     private final Button charger = new Button("Charger");
+    private final Button inscription = new Button("Inscription");
     private final ChoiceBox<String> sessions = new ChoiceBox<>();
     private final TableView<Course> tableView = new TableView<Course>();
     private ArrayList<Course> listCours;
@@ -32,7 +41,6 @@ public class clientView {
     private final Text fomulaire = new Text("Formulaire d'inscription");
 
     public clientView() {
-
         TableColumn<Course, String> codeCol = new TableColumn<>("Code");
         TableColumn<Course, String> coursCol = new TableColumn<>("Cours");
         codeCol.setCellValueFactory(new PropertyValueFactory<>("code"));
@@ -55,21 +63,42 @@ public class clientView {
 
             }
         });
-        tableView.setMaxWidth(200);
+        inscription.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                String pren = prenom.getText();
+
+            }
+        });
+        tableView.setMaxWidth(220);
         tableView.setMaxHeight(300);
         tableView.setTranslateX(-100);
         tableView.setTranslateY(-60);
-        codeCol.setMaxWidth(150);
-
+        codeCol.setMaxWidth(160);
+        coursCol.setMinWidth(160);
+        informations.getChildren().addAll(em,na,pr,prenom,nom,email,matricule,ma);
+        pr.setTranslateX(-50);
+        pr.setTranslateY(30);
+        na.setTranslateY(95);
+        na.setTranslateX(-50);
+        em.setTranslateX(-50);
+        em.setTranslateY(155);
+        ma.setTranslateX(-60);
+        ma.setTranslateY(-30);
+        informations.setMaxWidth(120);
+        informations.setTranslateX(145);
+        informations.setTranslateY(90);
 
         sessions.getItems().addAll("Automne", "Hiver", "Ete");
         sessions.setValue("Automne");
         sessions.setTranslateX(-100);
         sessions.setTranslateY(140);
         charger.setTranslateY(140);
+        inscription.setTranslateX(170);
+        inscription.setTranslateY(140);
         fomulaire.setTranslateX(140);
         fomulaire.setTranslateY(-200);
-        root.getChildren().addAll(tableView,charger,sessions,fomulaire);
+        root.getChildren().addAll(informations,tableView,charger,inscription,sessions,fomulaire);
         scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
 
 
@@ -79,6 +108,7 @@ public class clientView {
         tableView.setItems(FXCollections.observableArrayList(cours));
 
     }
+
 
     public Scene getScene(){
         return scene;
